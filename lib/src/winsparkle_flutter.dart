9aaf368131e4ea6107f645c26602220e821d2980
Herbert Poul <herbert@codeux.design>
2020-07-17 12:06:09 +0200

    export all win sparkle functions.

--- lib/src/winsparkle_flutter.dart
+++ lib/src/winsparkle_flutter.dart
@@ -1,1 +1,7 @@
+import 'dart:ffi';
+
+import 'package:ffi/ffi.dart';
+
+part 'typedefs.dart';
+
 final _winsparkle = DynamicLibrary.open('advapi32.dll');
--- lib/winsparkle_flutter.dart
+++ lib/winsparkle_flutter.dart
@@ -1,14 +1,1 @@
-
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class WinsparkleFlutter {
-  static const MethodChannel _channel =
-      const MethodChannel('winsparkle_flutter');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
+export 'src/winsparkle_flutter.dart';
